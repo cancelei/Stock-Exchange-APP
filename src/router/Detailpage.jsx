250ad47e7bbe87e2a9ab1533439510c492dc8ba2
Glauber <glauber.bannwart@gmail.com>
2023-08-17 20:23:50 -0300

    Update Stock Details Layout

--- src/router/Detailpage.jsx
+++ src/router/Detailpage.jsx
@@ -1,88 +1,71 @@
-import { Link, useParams } from 'react-router-dom';
-import { Col, Container, Row } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
+import { useParams } from 'react-router-dom';
+import { Container, Table } from 'react-bootstrap';
 import Navbar from '../components/NavElement/NavElement';
+import './DetailPage.css'; // Importing the CSS
 
 export default function Detailpage() {
   const {
     symbol, name, price, change, dayLow, dayHigh, marketCap, volume, exchange,
   } = useParams();
+
   return (
-    <main>
+    <main className="detail-main">
       <Navbar />
-      <Container
-        className="d-flex"
-        style={{
-          height: '80vh', backgroundColor: 'orange', color: 'white', fontSize: '2rem',
-        }}
-      >
-        <Col md={8}>
-          <Row className="d-flex row-cols-2" style={{ backgroundColor: '#5788e4', width: '' }}>
-            <div className="d-flex row-col-2" style={{ height: '8rem' }}>
-              <h2 style={{ margin: 'auto' }}>Stock Detail</h2>
-            </div>
-            <div style={{ margin: 'auto' }}>
-              <h3>{name}</h3>
-              <h3>
-                Ticker:
-                {' '}
-                {symbol}
-              </h3>
-            </div>
-          </Row>
-          <Row className="d-flex justify-content-center">
-            <p>
-              Price:
-              {' '}
-              {price}
-            </p>
-          </Row>
-          <Row className="d-flex justify-content-center">
-            <p>
-              Daily change:
-              {' '}
-              {change}
-            </p>
-          </Row>
-          <Row className="d-flex justify-content-center">
-            <p>
-              Min value today:
-              {' '}
-              {dayLow}
-            </p>
-          </Row>
-          <Row className="d-flex justify-content-center">
-            <p>
-              Max value today:
-              {' '}
-              {dayHigh}
-            </p>
-          </Row>
-          <Row className="d-flex justify-content-center">
-            <p>
-              Market Cap:
-              {' '}
-              {marketCap}
-            </p>
-          </Row>
-          <Row className="d-flex justify-content-center">
-            <p>
-              Vol:
-              {' '}
-              {volume}
-            </p>
-          </Row>
-          <Row className="d-flex justify-content-center">
-            <p>
-              Exchange:
-              {' '}
-              {exchange}
-            </p>
-          </Row>
-          <Link to="/" className="d-flex justify-content-center">
-            <Button type="submit" variant="secondary" className="mt-3"> &crarr; Close</Button>
-          </Link>
-        </Col>
+      <Container className="detail-container">
+        <h2 className="text-center mb-4">
+          Stock Detail for
+          {' '}
+          {name}
+          (
+          {symbol}
+          )
+        </h2>
+        <Table striped bordered hover className="detail-table">
+          <tbody>
+            <tr>
+              <td>Price</td>
+              <td>
+                {price}
+              </td>
+            </tr>
+            <tr>
+              <td>Daily change</td>
+              <td>
+                {change}
+              </td>
+            </tr>
+            <tr>
+              <td>Min value today</td>
+              <td>
+                {dayLow}
+              </td>
+            </tr>
+            <tr>
+              <td>Max value today</td>
+              <td>
+                {dayHigh}
+              </td>
+            </tr>
+            <tr>
+              <td>Market Cap</td>
+              <td>
+                {marketCap}
+              </td>
+            </tr>
+            <tr>
+              <td>Volume</td>
+              <td>
+                {volume}
+              </td>
+            </tr>
+            <tr>
+              <td>Exchange</td>
+              <td>
+                {exchange}
+              </td>
+            </tr>
+          </tbody>
+        </Table>
       </Container>
     </main>
   );
